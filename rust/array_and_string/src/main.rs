1057c2ff88279ec45d38b10d37776ff463b75330
k1r91 <cherkasov.kirill@gmail.com>
2022-06-08 11:58:36 +0500

    Refactor name

--- rust/array_and_string/src/main.rs
+++ rust/array_and_string/src/main.rs
@@ -1,10 +1,10 @@
-mod pivot_index;
+mod solution;
 
 fn main() {
     let nums= vec![1, 2, 3];
-    println!("{}", pivot_index::Solution::pivot_index(nums));
+    println!("{}", solution::Solution::pivot_index(nums));
     let nums= vec![1,7,3,6,5,6];
-    println!("{}", pivot_index::Solution::pivot_index(nums));
+    println!("{}", solution::Solution::pivot_index(nums));
     let nums= vec![2,1,-1];
-    println!("{}", pivot_index::Solution::pivot_index(nums));
+    println!("{}", solution::Solution::pivot_index(nums));
 }
